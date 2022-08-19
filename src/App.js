8abcd05c5eea136868c246fc9b102fb7b339b86e
Yusuf Engin Tetik <yusufengintetik@gmail.com>
2022-08-19 20:05:55 +0300

    defining and using routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+// A simple app that uses react router
+// npm install react-router-dom@5
+
+import { Route } from "react-router-dom";
+import Welcome from "./components/Welcome";
+import Products from "./components/Products";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
     </div>
   );
 }
 
 export default App;
+
+// our-domain.com/ => Component A
+// our-domain.com/products => Component B
